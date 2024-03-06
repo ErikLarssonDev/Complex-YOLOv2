5c1479ff9c30ea39b75a246b345715928adbb35a
Student <erik.larsson@magna.com>
2024-03-06 17:03:58 +0100

    Running experiment on 1024x512 with trainval data to track training loss for each iteration

--- config.py
+++ config.py
@@ -26,16 +26,16 @@ boundary = {
     "maxZ": 3
 }
 
-BEV_WIDTH = 3200 # 3200 # 4800 # Original: 1024 X [0, 40] 
-BEV_HEIGHT = 640 # 640 # 960 # Original: 512 Y [-40, 40] 
+BEV_WIDTH = 1024 # 3200 # 3200 # 4800 # Original: 1024 X [0, 40] 
+BEV_HEIGHT = 512 # 640 # 640 # 960 # Original: 512 Y [-40, 40] 
 # Cell size: 1024 / 80 = 12.8 --> 100 / 12.8 = 8 # Original: about 8cm
 # Cell size: 3200 / 250 = 12.8 --> 100 / 12.8 = 8 # Original: about 8cm
-# Cell size: 4800 / 250 = 19.2 --> 100 / 19.2 = 5.2 
+# Cell size: 4800 / 250 = 19.2 --> 100 / 19.2 = 5.2 cm
 
 
 CONFIG = {
-       "name": "3200x640",
-       "architecture": "Complex-YOLOv2-3200x640",
+       "name": "YOLO_5e_1024x512_trainval",
+       "architecture": "Complex-YOLOv2-4800x960_trainval",
        "dataset": './zod',
        "epochs": 5,
        "batch_size": 1,
